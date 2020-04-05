7cfadcbc6d95fb4584dbe3c51ca10a6d8bc27b28
LesterCerioli <lesterlucasit@hotmail.com>
2020-04-05 09:45:02 -0300

    Authentication structure implementation/buik from Html pages

---
+++ LucasTecBlazor.Web/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LucasTecBlazor.Web.Areas.Identity
+{
+    public class RevalidatingIdentityAuthenticationStateProvider
+    {
+    }
+}
